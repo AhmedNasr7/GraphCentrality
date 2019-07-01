1b0ef7d0e8ce879dda6c2f8ffed6a1d1f6132b1d
Lamees Emad <lameesemad1996@hotmail.com>
2019-07-02 00:16:20 +0200

    used an adjacency matrix instead of an adjacency list in degree centrality analysis

--- Algorithms/DegreeCentrality/DegreeCentrality.py
+++ Algorithms/DegreeCentrality/DegreeCentrality.py
@@ -1,46 +1,29 @@
-def degree_centrality(graph_list, index):
-    return len(graph_list[index])
+def degree_centrality(adj_mat, index):
+    count = 0
+    for p in range(number_nodes):
+        if adj_mat[index][p] == 1:
+            count = count + 1
+    return count
 
 
-def print_centrality(graph_list):
-    for i in range(len(graph_list)):
-        print(str(degree_centrality(graph_list, i)) + "\n")
+def print_centrality(adj_mat):
+    for n in range(number_nodes):
+        print(str(degree_centrality(adj_mat, n)) + "\n")
     return None
-
-
-def parse_input():
-    first_line = input()
-    first_line = first_line.split()
-    number_nodes = int(first_line[0])
-    number_edges = int(first_line[1])
-    graph = [[] for j in range(number_nodes)]
-    # initialization for graph
-    for i in range(number_edges):
-        my_line = lines[i + 1]
-        my_line = my_line.split()
-        my_line = [int(i) for i in my_line]
-        if my_line[1] in graph[my_line[0]]:
-            continue
-        graph[my_line[0]].append([my_line[1], my_line[2]])
-        graph[my_line[1]].append([my_line[0], my_line[2]])
-        i = i + 1
-    return graph
 
 
 first_line = input()
 first_line = first_line.split()
 number_nodes = int(first_line[0])
 number_edges = int(first_line[1])
-graph = [[] for j in range(number_nodes)]
+adj_matrix = [[0 for x in range(number_nodes)] for y in range(number_nodes)]
 
 for i in range(number_edges):
     my_line = input()
     my_line = my_line.split()
     my_line = [int(i) for i in (my_line)]
-    if my_line[1] in graph[my_line[0]]:
-        continue
-    graph[my_line[0]].append([my_line[1], my_line[2]])
-    graph[my_line[1]].append([my_line[0], my_line[2]])
+    adj_matrix[my_line[0]][my_line[1]] = 1
+    adj_matrix[my_line[1]][my_line[0]]= 1
     i = i + 1
 
-print_centrality(graph)
+print_centrality(adj_matrix)
